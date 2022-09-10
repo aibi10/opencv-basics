cec4246bf56a66366e1d9848acce00102b31f02d
ABHISHEK SINGH <45706760+aibi10@users.noreply.github.com>
2022-09-10 07:23:48 +0530

    initial setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,10 +1,12 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
+conda create -n opencvbasics python=3.7 -y
 echo [$(date)]: "activate environment"
-source activate ./env
+source activate opencvbasics
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 echo [$(date)]: "install PyTorch"
 conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
+echo [$(date)]: "END"
+conda env export > conda.yaml
 echo [$(date)]: "END"
